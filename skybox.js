479b3d0d6a5d136677e254b78131735cf5cbd39e
Maude313 <maura@ruopsa.net>
2023-09-29 14:33:17 +0300

    warp function almost there, WIP

--- skybox.js
+++ skybox.js
@@ -14,6 +14,9 @@ document.addEventListener('DOMContentLoaded', function () {
   camera.position.z = 0.001; // Adjust the camera position as needed
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
+  const pixelRatio = window.devicePixelRatio || 1; // Use 1 as the default for standard displays
+  renderer.setPixelRatio(pixelRatio);
+  
   const cameraControls = new CameraControls( camera, renderer.domElement );
   // const cControls = CameraControls.install( { THREE: THREE } );
   // cControls = new CameraControls(camera, renderer.domElement);
@@ -24,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // sceneContainer.style.display = 'block'; // Show the content
   // const controls = new THREE.OrbitControls(camera, renderer.domElement);
   // controls.enableZoom = false;
+  cameraControls.enableZoom = false;
   // Load skybox textures
   const textureLoader = new THREE.TextureLoader();
   const skyTexture = textureLoader.load('sky4.jpg'); // jpg image for the background, the skybox image
@@ -58,13 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
   let changeDirectionIntervalX = 9000; // Interval to change direction (in milliseconds)
   let lastDirectionChange = performance.now();
 
-  // Camera rotation values for click event listener
-  let cameraRotationX = camera.rotation.x;
-  let cameraRotationY = camera.rotation.y;
-  let cameraRotationZ = camera.rotation.z;
-  let newCameraRotationX = cameraRotationX;
-  let newCameraRotationY = cameraRotationY;
-  let newCameraRotationZ = cameraRotationZ;
+
 
   //Add realism with damping, no abrupt stops
   // controls.enableDamping = true;
@@ -110,7 +108,7 @@ document.addEventListener('DOMContentLoaded', function () {
   };
   
       
-  animate();
+
   
   // window.addEventListener("keydown", function (event) {  
   //     if (event.key !== undefined) {
@@ -165,7 +163,14 @@ document.addEventListener('DOMContentLoaded', function () {
   `;
   // cameraControls.fitToSphere(skySphere, enableTransition);
   contentContainer.innerHTML = startContent;
-  let currentCameraRotation;
+  // Camera rotation values for click event listener
+  // let cameraRotationX = camera.rotation.x;
+  // let cameraRotationY = camera.rotation.y;
+  // let cameraRotationZ = camera.rotation.z;
+  // let newCameraRotationX;
+  // let newCameraRotationY;
+  // let newCameraRotationZ;
+  let warpInProcess;
   //Click events for buttons/links
   contentContainer.addEventListener('click', (event) => {
     // Check if the clicked element has a specific ID
@@ -177,19 +182,34 @@ document.addEventListener('DOMContentLoaded', function () {
         break;
       case 'link2':
         cameraControls.setLookAt( 0, 0, camera.position.z, 0, -0.1, 0.3, enableTransition )
-        // warp();
         // enableDamping = false;
-        newCameraRotationX = camera.rotation.x;
-        newCameraRotationY = camera.rotation.y;
-        newCameraRotationZ = camera.rotation.z;
+        const processState = 
+        // switch (processState)
+        warpInProcess = true;
+        console.log('switch case: warpInProcess:', warpInProcess);
+        if (warpInProcess) {
+          warp();
+          contentContainer.innerHTML = galleryContent;
+        }
+        else {
+        }
+        // setTimeout(() => {
+        //   warpInProcess = false;
+        //   console.log("setTimeout, warp in process " + warpInProcess);
+        //   return
+        // }, 3000); // this is the time you want to run the warp
         contentContainer.innerHTML = galleryContent;
+        
+        // newCameraRotationX = camera.rotation.x;
+        // newCameraRotationY = camera.rotation.y;
+        // newCameraRotationZ = camera.rotation.z;
         break;
       case 'link3':
-        cameraControls.setLookAt( 0, 0, camera.position.z, cameraRotationX, cameraRotationY, cameraRotationZ, enableTransition )
-        newCameraRotationX = camera.rotation.x;
-        newCameraRotationY = camera.rotation.y;
-        newCameraRotationZ = camera.rotation.z;
+        cameraControls.setLookAt( 0, 0, camera.position.z, 0, 0, 0, enableTransition )
         contentContainer.innerHTML = startContent;
+        // newCameraRotationX = camera.rotation.x;
+        // newCameraRotationY = camera.rotation.y;
+        // newCameraRotationZ = camera.rotation.z;
         break;
       case 'toggle_full_screen':
         break;
@@ -203,15 +223,8 @@ document.addEventListener('DOMContentLoaded', function () {
         `
         break;
     }
-    if (targetId !== 'link1') {
-
-      camera.rotation.set(newCameraRotationX, newCameraRotationY, newCameraRotationZ);
-    }
-    // currentCameraRotation = camera.rotation;
-    console.log("newCameraRotation " + newCameraRotationX + ", " + newCameraRotationY + ", " + newCameraRotationZ);
-    // console.log("controls target " + controls.target.position);
-
     contentContainer.innerHTML;
+    animate();
 
     // Get a reference to the full-screen button with its new ID (reference is lost after uploading new content)
     const fullScreenButtonNew = document.getElementById('toggle_full_screen');
@@ -261,6 +274,481 @@ document.addEventListener('DOMContentLoaded', function () {
 
   //-----------------------------WARP----------------------------
 
+
+
+
+  var starHolderCount = 6666;
+  var starHolder = [];
+  var starBgHolder = [];
+  var starSpeed = 20;
+  var starSpeedMin = starSpeed;
+  var starSpeedMax = 200;
+  var starDistance = 8000;
+  var starRotation = 0;
+
+  var backgroundColor = { r:0, g:0, b:0, a:0 };
+
+  var colorInvertValue = 0;
+  function warp(callback) {
+    if (!warpInProcess) {
+      console.log("warp has ended");
+      return
+    }
+    else {
+      console.log("warping function started");
+      $( document ).ready( function() {
+
+        var isMobile = false;
+
+        //---
+
+        if ( isMobile.phone || isMobile.tablet ) {
+
+          isMobile = true;
+
+        }
+
+        //---
+
+        var canvas = document.createElement( 'canvas' );
+        var canvasWidth = window.innerWidth;
+        var canvasHeight = window.innerHeight;
+
+        //---
+
+        canvas.setAttribute( 'width', canvasWidth );
+        canvas.setAttribute( 'height', canvasHeight );
+        canvas.oncontextmenu = function( e ) {
+          e.preventDefault();
+        };
+
+        const effect = document.getElementById( 'effect' ).appendChild( canvas );
+        effect.style.backgroundColor = 'transparent';
+        //---
+
+        var ctx = canvas.getContext( '2d' );
+        var imageData = ctx.getImageData( 0, 0, canvasWidth, canvasHeight );
+        var pix = imageData.data;
+
+        //---
+
+        var MATHPI180 = Math.PI / 180;
+        var MATHPI2 = Math.PI * 2;
+
+        var center = { x:canvas.width / 2, y:canvas.height / 2 };
+
+        //---
+
+        var mouseActive = false;
+        var mouseDown = false;
+        var mousePos = { x:center.x, y:center.y };
+
+        //---
+
+        var rotationSpeed = -1.00;
+        var rotationSpeedFactor = { x: 0, y: 0 };
+        rotationSpeedFactor.x = rotationSpeed / center.x;
+        rotationSpeedFactor.y = rotationSpeed / center.y;
+
+        var fov = 300;
+        var fovMin = 210;
+        var fovMax = fov;
+
+  
+
+        //---
+
+        function clearImageData() {
+
+          for ( var i = 0, l = pix.length; i < l; i += 4 ) {
+
+            pix[ i ] = backgroundColor.r;
+            pix[ i + 1 ] = backgroundColor.g;
+            pix[ i + 2 ] = backgroundColor.b;
+            pix[ i + 3 ] = backgroundColor.a;
+
+          }
+
+        };
+
+        function setPixel( x, y, r, g, b, a ) {
+
+          var i = ( x + y * canvasWidth ) * 4;
+
+          pix[ i ] = r;
+          pix[ i + 1 ] = g;
+          pix[ i + 2 ] = b;
+          pix[ i + 3 ] = a;
+
+        };
+
+        function setPixelAdditive( x, y, r, g, b, a ) {
+
+          var i = ( x + y * canvasWidth ) * 4;
+
+          pix[ i ]     = pix[ i ] + r;
+          pix[ i + 1 ] = pix[ i + 1 ] + g;
+          pix[ i + 2 ] = pix[ i + 2 ] + b;
+          pix[ i + 3 ] = a;
+
+        };
+
+        //---
+
+        function drawLine( x1, y1, x2, y2, r, g, b, a ) {
+
+          var dx = Math.abs( x2 - x1 );
+          var dy = Math.abs( y2 - y1 );
+
+          var sx = ( x1 < x2 ) ? 1 : -1;
+          var sy = ( y1 < y2 ) ? 1 : -1;
+
+          var err = dx - dy;
+
+          var lx = x1;
+          var ly = y1;    
+
+          while ( true ) {
+
+            if ( lx > 0 && lx < canvasWidth && ly > 0 && ly < canvasHeight ) {
+
+              setPixel( lx, ly, r, g, b, a );
+
+            }
+
+            if ( ( lx === x2 ) && ( ly === y2 ) )
+              break;
+
+            var e2 = 2 * err;
+
+            if ( e2 > -dx ) { 
+
+              err -= dy; 
+              lx += sx; 
+
+            }
+
+            if ( e2 < dy ) { 
+
+              err += dx; 
+              ly += sy; 
+
+            }
+
+          }
+
+        };
+
+        //---
+
+        function addParticle( x, y, z, ox, oy, oz ) {
+
+          var particle = {};
+          particle.x = x;
+          particle.y = y;
+          particle.z = z;
+          particle.ox = ox;
+          particle.oy = oy;
+          particle.x2d = 0;
+          particle.y2d = 0;
+
+          return particle;
+
+        };
+
+        function addParticles() {
+
+          var i;
+
+          var x, y, z;
+
+          var colorValue;
+          var particle;
+
+          for ( i = 0; i < starHolderCount / 3; i++ ) {
+
+            x = Math.random() * 24000 - 12000;
+            y = Math.random() * 4500 - 2250;
+            z = Math.round( Math.random() * starDistance );//Math.random() * 700 - 350;
+
+            colorValue = Math.floor( Math.random() * 55 ) + 5;
+
+            particle = addParticle( x, y, z, x, y, z );
+            particle.color = { r:colorValue, g:colorValue, b:colorValue, a:255 };
+
+            starBgHolder.push( particle );
+
+          }
+
+          for ( i = 0; i < starHolderCount; i++ ) {
+
+            x = Math.random() * 10000 - 5000;
+            y = Math.random() * 10000 - 5000;
+            z = Math.round( Math.random() * starDistance );//Math.random() * 700 - 350;
+
+            colorValue = Math.floor( Math.random() * 155 ) + 100;
+
+            particle = addParticle( x, y, z, x, y, z );
+            particle.color = { r:colorValue, g:colorValue, b:colorValue, a:255 };
+            particle.oColor = { r:colorValue, g:colorValue, b:colorValue, a:255 };
+            particle.w = 1;
+            particle.distance = starDistance - z;
+            particle.distanceTotal = Math.round( starDistance + fov - particle.w );
+
+            starHolder.push( particle );
+
+          }
+
+        };
+
+        //---
+
+        window.requestAnimFrame = ( function() {
+
+          return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            function( callback ) {
+            window.setTimeout( callback, 1000 / 60 );
+          };
+
+        } )();
+
+        function animloop() {
+
+          requestAnimFrame( animloop );
+          render();
+
+        };
+
+        function render() {
+
+          clearImageData();
+
+          //---
+
+          var i, j, l, k, m, n;
+
+          //---
+
+          var rx, rz;
+
+          var star;
+          var scale;
+
+
+          // Get the animation start time
+          var animationStartTime = null;
+          
+          // starSpeed = 20; // Start with the minimum speed
+          function animate() {
+            // Define your starSpeed parameters
+            console.log("starspeed: " + starSpeed);
+    
+            // Set the animation duration in milliseconds
+            var animationDuration = 3000; // Adjust this value as needed
+            
+            if (!animationStartTime) {
+              animationStartTime = Date.now();
+            }
+            // warpInProcess = true;
+            
+            // Calculate the elapsed time
+            var elapsedTime = Date.now() - animationStartTime;
+            
+            
+            // Update starSpeed based on the elapsed time
+            if (elapsedTime < animationDuration / 2) {
+              // Accelerate during the first half of the animation
+              starSpeed = starSpeedMin + (2 * (starSpeedMax - starSpeedMin) * (elapsedTime / (animationDuration / 2)));
+            } else {
+              // Decelerate during the second half of the animation
+              starSpeed = starSpeedMax - (2 * (starSpeedMax - starSpeedMin) * ((elapsedTime - animationDuration / 2) / (animationDuration / 2)));
+            }
+
+              if (elapsedTime > animationDuration) {
+                canvas.remove();
+                console.log("canvas removed");
+                warpInProcess = false;
+                return
+              }
+              else {
+                contentContainer.innerHTML = `
+                <h1 class="header-text" id="header-text">OOOOOOPS</h1>
+                `;
+                requestAnimationFrame(animate);
+              }
+              return
+            }
+            // Start the animation
+            if (warpInProcess) {
+              requestAnimationFrame(animate);
+            }
+            warpInProcess = false;
+            //---
+
+          if ( !mouseActive ) {
+
+            fov += 0.5;
+
+            if ( fov > fovMax )
+              fov = fovMax;
+
+          } else {
+
+            fov -= 1;
+
+            if ( fov < fovMin )
+              fov = fovMin;
+
+          }
+
+          //---
+
+          var warpSpeedValue;
+
+          if ( isMobile ) {
+
+            warpSpeedValue = starSpeed * ( starSpeed / starSpeedMax );
+
+          } else {
+
+            warpSpeedValue = starSpeed * ( starSpeed / ( starSpeedMax / 2 ) );
+
+          }
+
+          //---
+
+          for ( i = 0, l = starBgHolder.length; i < l; i++ ) {
+
+            star = starBgHolder[ i ];
+
+            scale = fov / ( fov + star.z ); 
+
+            star.x2d = ( star.x * scale ) + center.x; 
+            star.y2d = ( star.y * scale ) + center.y; 
+
+            if ( star.x2d > 0 && star.x2d < canvasWidth && star.y2d > 0 && star.y2d < canvasHeight ) {
+
+              setPixel( star.x2d | 0, star.y2d | 0, star.color.r, star.color.g, star.color.b, 255 );
+
+            }
+
+
+          }
+
+          //---
+
+          for ( i = 0, l = starHolder.length; i < l; i++ ) {
+
+            star = starHolder[ i ];
+
+            star.z -= starSpeed;
+            star.distance += starSpeed;
+
+            if ( star.z < -fov + star.w ) {
+
+              star.z = starDistance;
+              star.distance = 0;
+
+            } 
+
+            //---
+            //star color
+
+            var distancePercent = star.distance / star.distanceTotal;
+
+            star.color.r = Math.floor( star.oColor.r * distancePercent );
+            star.color.g = Math.floor( star.oColor.g * distancePercent );
+            star.color.b = Math.floor( star.oColor.b * distancePercent );
+
+            //---
+            //star draw
+
+            scale = fov / ( fov + star.z ); 
+
+            star.x2d = ( star.x * scale ) + center.x; 
+            star.y2d = ( star.y * scale ) + center.y; 
+
+            if ( star.x2d > 0 && star.x2d < canvasWidth && star.y2d > 0 && star.y2d < canvasHeight ) {
+
+              setPixelAdditive( star.x2d | 0, star.y2d | 0, star.color.r, star.color.g, star.color.b, 255 );
+
+            }
+
+            if ( starSpeed != starSpeedMin ) {
+
+              var nz = star.z + warpSpeedValue;
+
+              scale = fov / ( fov + nz ); 
+
+              var x2d = ( star.x * scale ) + center.x; 
+              var y2d = ( star.y * scale ) + center.y; 
+
+              if ( x2d > 0 && x2d < canvasWidth && y2d > 0 && y2d < canvasHeight ) {
+
+                drawLine( star.x2d | 0, star.y2d | 0, x2d | 0, y2d | 0, star.color.r, star.color.g, star.color.b, 255 );
+
+              }
+
+            }
+
+            if ( mouseDown ) {
+
+              //rotation
+              var radians = MATHPI180 * starRotation;
+
+              var cos = Math.cos( radians );
+              var sin = Math.sin( radians );
+
+              star.x = ( cos * ( star.ox - center.x ) ) + ( sin * ( star.oy - center.y ) ) + center.x,
+                star.y = ( cos * ( star.oy - center.y ) ) - ( sin * ( star.ox - center.x ) ) + center.y;
+
+            }
+
+          }
+
+          //---
+
+          ctx.putImageData( imageData, 0, 0 );
+
+          //---
+
+          if ( mouseActive ) {
+
+            center.x += ( mousePos.x - center.x ) * 0.015;
+
+          } else {
+
+            center.x += ( ( canvas.width / 2 ) - center.x ) * 0.015;
+
+          }
+
+          //---
+
+          if ( mouseDown ) {
+
+            starRotation -= 0.5;
+
+          }
+
+        };
+
+
+        //---
+        if (warpInProcess) {
+          addParticles();
+          animloop();
+        }
+      })
+       
+    }
+  }
+});
+
+
+
+  //-----------------------------end--------------------------------
 
 //   function warp() {
 
@@ -689,7 +1177,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
 //               drawLine( star.x2d | 0, star.y2d | 0, x2d | 0, y2d | 0, star.color.r, star.color.g, star.color.b, 255 );
     
-//             }
+//             }imageData
     
 //           }
 
@@ -714,6 +1202,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //       });
 
-//   }
-
-});
